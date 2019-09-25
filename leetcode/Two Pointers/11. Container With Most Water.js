0673d38d8c950641e3839c791395094f1e5ba423
Yu Zong <yuzong@yus-macbook-pro.attlocal.net>
2019-09-24 23:20:27 -0700

    11. Container With Most Water

---
+++ leetcode/Two Pointers/11. Container With Most Water.js
@@ -0,0 +1,17 @@
+var maxArea = function(height) {
+    let max = 0;
+    let left = 0;
+    let right = height.length - 1;
+
+    while (left < right) {
+        let area = (right - left) * Math.min(height[left], height[right]);
+        max = Math.max(max, area);
+
+        if (height[left] < height[right]) {
+            left ++;
+        } else {
+            right --;
+        }
+    }
+    return max;
+};
