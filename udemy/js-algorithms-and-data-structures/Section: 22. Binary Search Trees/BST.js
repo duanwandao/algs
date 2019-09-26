90fae9fbe2252b170a7135fdc10013a31fc0e88a
Yu Zong <yuzong@yus-macbook-pro.attlocal.net>
2019-09-26 01:04:36 -0700

    BST

--- udemy/js-algorithms-and-data-structures/Section: 22. Binary Search Trees/BST.js
+++ udemy/js-algorithms-and-data-structures/Section: 22. Binary Search Trees/BST.js
@@ -16,50 +16,44 @@ class BinarySearchTree {
   }
   insert(value) {
     const newNode = new Node(value);
-    if (this.root === null) {
+    if (!this.root) {
       this.root = newNode;
       return this;
     }
     let current = this.root;
     while (true) {
-      if (value === current.value) {
-        return undefined;
-      }
       if (value < current.value) {
-        if (current.left === null) {
+        if (!current.left) {
           current.left = newNode;
           return this;
-        } else {
-          current = current.left;
         }
+        current = current.left;
       } else {
-        if (current.right === null) {
+        if (!current.right) {
           current.right = newNode;
           return this;
-        } else {
-          current = current.right;
         }
+        current = current.right;
       }
     }
   }
 
   find(value) {
-    if (this.root === null) {
+    if (!this.root) {
       return false;
     }
     let current = this.root;
-    let found = false;
-    while (current && !found) {
+
+    while (current) {
       if (value < current.value) {
         current = current.left;
       } else if (value > current.value) {
         current = current.right;
       } else {
-        found = true;
+        return current;
       }
     }
-    if(!found) return undefined;
-    return current;
+    return false;
   }
 
   BFS() {
@@ -143,11 +137,11 @@ class BinarySearchTree {
 
 var tree = new BinarySearchTree();
 tree.insert(10);
-tree.insert(5)
-tree.insert(13)
-tree.insert(11)
-tree.insert(2)
-tree.insert(16)
-tree.insert(7)
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
 
 console.log(tree.DFSInOrder());
